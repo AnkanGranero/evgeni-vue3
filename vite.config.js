a4b953d67b2672fe9cd7bc8e0c5388e94789694e
Andreas Graner <grannemannen@gmail.com>
2023-06-26 23:33:16 +0200

    plugin vite rewrite all

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-/* import pluginRewriteAll from 'vite-plugin-rewrite-all' */
+import pluginRewriteAll from 'vite-plugin-rewrite-all' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
         outDir: 'dist',
     },
   publicDir: 'assets',
-  plugins: [vue()], 
+  plugins: [vue(), pluginRewriteAll()], 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
