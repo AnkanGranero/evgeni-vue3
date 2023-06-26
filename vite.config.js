5256fe0919a371bfb4b4c47a209a2d0f13873ecf
Andreas Graner <grannemannen@gmail.com>
2023-06-26 22:35:00 +0200

    vite url fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
         outDir: 'dist',
     },
   publicDir: 'assets',
-  plugins: [vue()],
+  plugins: [vue(), pluginRewriteAll()], 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
