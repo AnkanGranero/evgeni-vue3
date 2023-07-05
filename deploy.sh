ae697de393283f281128f092b603f43be03c1304
Andreas Graner <grannemannen@gmail.com>
2023-07-05 13:14:05 +0200

    deploy kommentar

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,8 @@ git add -A
 git commit -m "deploy"
 git push -f git@github.com:AnkanGranero/evgeni-vue3.git main:gh-pages
 
+
+använd denna istället "git subtree push --prefix dist origin gh-pages"
+men du kan behöva ta bort github branchen innan du pushar ( finns såklart bättre lösning)
+
 cd -
