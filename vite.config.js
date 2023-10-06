8568c7a2b0a7f23f8753fe876dfea915a0f58ade
Andreas Graner <grannemannen@gmail.com>
2023-10-06 16:01:55 +0200

    added base to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/evgeni-vue3/',
   root: './',
       build: {
         outDir: 'dist',
