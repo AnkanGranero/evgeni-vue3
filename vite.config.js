1b4b952a1a9cd39a21a96cf537125c1c634a9f45
Andreas Graner <grannemannen@gmail.com>
2024-04-08 22:06:17 +0200

    ändrad vite config

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,15 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/evgeni-vue3/',
+  base: '/',
   root: './',
       build: {
         outDir: 'dist',
     },
   publicDir: 'assets',
-  plugins: [vue(), pluginRewriteAll()], 
+  plugins: [vue(), pluginRewriteAll({
+      endpoint: 'index.html' // Konfigurera pluginet för att omdirigera alla förfrågningar till index.html
+    })], 
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) } ]
