e12aaccb8f960ad819313d6af4e05286dd699cb5
Andreas Graner <grannemannen@gmail.com>
2024-04-08 21:50:21 +0200

    image for presentation from CMS

--- src/views/Hej.vue
+++ src/views/Hej.vue
@@ -2,21 +2,24 @@
 import fetchFromContentful from "@/helpers/helperfunctions.js";
 import { ref, onMounted, computed } from "vue";
 const presentationsText = ref();
+const headerImage = ref();
 onMounted(async () => {
   try {
     await fetchFromContentful("presentation")
       .then((resp) =>
         resp.map((item) => {
           let { fields } = item;
-
-          let updatedElement = fields.presentationstext.content.map(
+          let updatedElement = {};
+          updatedElement.text = fields.presentationstext.content.map(
             (item) => item.content[0].value
           );
+          updatedElement.headerImage = fields.headerImage;
           return updatedElement;
         })
       )
       .then((resp) => {
-        presentationsText.value = resp[0];
+        presentationsText.value = resp[0].text;
+        headerImage.value = resp[0].headerImage.fields.file.url;
       });
   } catch (error) {
     throw error;
@@ -28,7 +31,7 @@ onMounted(async () => {
     <h1>Hej!</h1>
     <div class="body-wrapper">
       <div class="body-content">
-        <img src="@/assets/Evgeni-turkos.jpeg" id="hej" />
+        <img :src="headerImage" id="hej" />
 
         <div v-for="(text, index) in presentationsText" :key="index">
           <p>{{ text }}</p>
@@ -40,10 +43,23 @@ onMounted(async () => {
 
 
 
-<style scoped>
+<style lang="scss" scoped>
 img {
   width: 100%;
   background: black;
   margin-bottom: 10%;
+  width: 100%;
+
+  object-fit: cover;
+  object-position: top;
+  max-height: 80vh;
+
+  @media only screen and (max-width: $mobile) {
+    max-height: 60vh;
+    background: black;
+    margin-bottom: 10%;
+    object-fit: cover;
+    object-position: top;
+  }
 }
 </style>
